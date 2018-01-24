5193da2a26bf5a95737539be53009f70006cebb5
yuda <yuda@ksccb.com>
2018-01-24 15:28:39 +0700

    initial commit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    {{test}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  data() {
+    return {
+      test : 'yuda'
+    }
+  }
+}
+</script>
+
+<style>
+  .home {
+    height: 50em;
+    border: solid;
+  }
+</style>
